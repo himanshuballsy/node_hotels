14945199ae86d3876b84559d7504283ae1ea1519
Your Name <himanshubanduni783@gmail.com>
2024-11-07 20:09:11 +0530

    shift ho gye mongodb se onlie server tak

--- db.js
+++ db.js
@@ -1,14 +1,16 @@
-
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const mongoURL = 'mongodb://localhost:27017/hotels';
+ //const mongoURL = 'mongodb://localhost:27017/hotels';
+const mongoURL = process.env.MONGODB_URL
 
 mongoose.connect(mongoURL);
 
 const db = mongoose.connection;
 
+
 db.on('connected', () => {
-    console.log('ho gaya bhai bhai jai baba kii');
+    console.log('connect ho gaya bhai jai baba kii');
 });
 
 db.on('error', () => {
--- iindex.js
+++ iindex.js
@@ -2,19 +2,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const db = require('./db'); // This should connect to MongoDB // This should correctly import your Person model
-
+require('dotenv').config();
 // Middleware to parse JSON data
 app.use(bodyParser.json());
-
+const PORT = process.env.PORT || 3000
 
 
 
 // Import the router and mount it
 const logokaroutes = require('./router/route'); // Correctly points to the router fil
-app.use('/buildo', logokaroutes);
+app.use('/person', logokaroutes);
+
 
 // Start the server on port 3000
 //aaja bilo ranni
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
